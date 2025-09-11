1f469c12a9f5a1f6c2fe2c47f178352577ff58dc
Alaynna Rodoff <arodoff@clarityinnovates.com>
2025-09-10 22:53:06 -0400

    git commit

---
+++ backend1-api/backend1-api/src/test/java/clarity/internship/backend1_api/Backend1ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package clarity.internship.backend1_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend1ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
