5589274687e7fed37330bffd8d6d589618a9f716
Alaynna Rodoff <arodoff@clarityinnovates.com>
2025-09-22 09:39:42 -0400

    week 2 commit - "Add password hashing and user existence check"

---
+++ backend-api/src/main/java/clarity/internship/backend_api/data/UserRepository.java
@@ -0,0 +1,10 @@
+package clarity.internship.backend_api.data;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import clarity.internship.backend_api.models.User;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    public User findOneByUsername(String username);
+
+}
