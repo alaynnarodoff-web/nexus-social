ebd10ad1a270e9afe966ffaecd8aeb7258f91769
Alaynna Rodoff <arodoff@clarityinnovates.com>
2025-09-24 11:49:37 -0400

    Commit 9/24/25 Adding user creation page and thumbnail image

--- backend-api/src/main/java/clarity/internship/backend_api/models/User.java
+++ backend-api/src/main/java/clarity/internship/backend_api/models/User.java
@@ -10,6 +10,11 @@ public class User {
     @Id
     private String id;
     private String username = "";
+    private String email = "";
+    private String firstName = "";
+    private String lastName = "";
+    private String phoneNumber = "";
+    private String avatar = "";
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password = "";
 
@@ -35,6 +40,46 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
     }
 
 }
